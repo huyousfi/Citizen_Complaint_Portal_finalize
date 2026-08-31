10fe015f7c0d7aa658d561b3f9d03dbedad437ab
huyousfi <huyousfi@gmail.com>
2026-08-31 05:55:01 +0500

    ss

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ const connectDB = async () => {
 
   const mongoUri =
     process.env.MONGODB_URI ||
-    'mongodb://huyousfi_db_user:oSWUJL1bsQFBULOb@ac-2vdhjbn-shard-00-00.bphjqsx.mongodb.net:27017,ac-2vdhjbn-shard-00-01.bphjqsx.mongodb.net:27017,ac-2vdhjbn-shard-00-02.bphjqsx.mongodb.net:27017/citizen_portal?ssl=true&replicaSet=atlas-u256pr-shard-0&authSource=admin&appName=Cluster0'
+    'mongodb+srv://huyousfisoft_db_user:YKIiHFGHj0tDEE6r@cluster0.jmvgzzy.mongodb.net/citizen_portal?appName=Cluster0&retryWrites=true&w=majority'
 
   await mongoose.connect(mongoUri)
 }
